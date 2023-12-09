5305c456559a822cb42980778589e5092cd2c922
Nabil Kachar <kacharnabil1@gmail.com>
2023-12-09 18:07:22 +0100

    update invoice feature has been updated

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,7 +31,31 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 
+
+  
     // Test it out:
     console.log(date);
     console.log(typeof amount);
+};
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
